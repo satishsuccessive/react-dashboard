89187c29504791bf88b7451cabf306c9f379ecaf
satishmaurya <satish.maurya@dentsuaegis.com>
2020-09-22 16:32:23 +0530

    Updates

---
+++ precache-manifest.7f564b085d0fb30d5ce5622a1d5d69a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6a4410c76272f3786ebde6eb03e10f3",
+    "url": "/react-dashboard/index.html"
+  },
+  {
+    "revision": "9bc0176763444a350da9",
+    "url": "/react-dashboard/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "45f7cd088038a85e169b",
+    "url": "/react-dashboard/static/css/main.0167cfdf.chunk.css"
+  },
+  {
+    "revision": "9bc0176763444a350da9",
+    "url": "/react-dashboard/static/js/2.46bbb688.chunk.js"
+  },
+  {
+    "revision": "b8bb29d6860b81155b6603b31c5ad0c1",
+    "url": "/react-dashboard/static/js/2.46bbb688.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45f7cd088038a85e169b",
+    "url": "/react-dashboard/static/js/main.1b50b5f5.chunk.js"
+  },
+  {
+    "revision": "7251f87bcc8a7d938ae1",
+    "url": "/react-dashboard/static/js/runtime-main.b88f03ae.js"
+  }
+]);
